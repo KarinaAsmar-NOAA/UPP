aa2082e9f171e638eae600a042a4bdba44d413d4
KarinaAsmar-NOAA <148993962+KarinaAsmar-NOAA@users.noreply.github.com>
2024-09-09 07:31:20 -0400

    add cosl

--- sorc/ncep_post.fd/CALCHIPSI.f
+++ sorc/ncep_post.fd/CALCHIPSI.f
@@ -269,7 +269,8 @@
               if(VP(ip1,J)==SPVAL .or. VP(im1,J)==SPVAL .or. &
                  UP(I,J-1)==SPVAL .or. UP(I,J+1)==SPVAL) cycle
               ! PSI(I,J) = (UP(I,J-1)*COSL(I,J-1)-UP(I,J+1)*COSL(I,J+1))*wrk3(i,j)
-              PSI(I,J) = -1/2*(VP(im1,J)+VP(ip1,J))*wrk2(i,j) + 1/2*(UP(I,J+1)+UP(I,J-1))*wrk3(i,j)
+              PSI(I,J) = -1/2*(VP(im1,J)+VP(ip1,J))*wrk2(i,j) + 1/2*(UP(I,J+1)*COSL(I,J+1)      &
+      &              + UP(I,J-1)*COSL(I,J-1))*wrk3(i,j)
               CHI(I,J) = (VP(I,J-1)*COSL(I,J-1)-VP(I,J+1)*COSL(I,J+1))*wrk3(i,j)
             ENDDO
           END IF                              ! END J IF BLOCK
