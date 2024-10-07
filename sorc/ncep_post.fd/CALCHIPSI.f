cedbcdf732ef90a53984efd590f14dbca5e172f0
KarinaAsmar-NOAA <148993962+KarinaAsmar-NOAA@users.noreply.github.com>
2024-10-07 12:41:08 -0400

    rename ip1 and im1 to i+/-1

--- sorc/ncep_post.fd/CALCHIPSI.f
+++ sorc/ncep_post.fd/CALCHIPSI.f
@@ -107,15 +107,15 @@
                   im1 = iw(i)
                   ii = i + imb2
                   if (ii > im) ii = ii - im
-                  psi_out(ip1,j) = dpsi_full(I,J) + psi_out(im1,J-1) 
-                  chi_out(ip1,j) = dchi_full(I,J) + chi_out(im1,J-1)                
+                  psi_out(i+!,j) = dpsi_full(I,J) + psi_out(i-1,J-1) 
+                  chi_out(i+1,j) = dchi_full(I,J) + chi_out(im1,J-1)                
                 enddo
               ELSE                                   !pole point,compute at jm-1
                 jj = jm-1
                 DO I=2,IM-1
                   ip1 = ie(i)
                   im1 = iw(i)
-                  psi_out(ip1,jj) = dpsi_full(I,J) + psi_out(im1,jj-1)
+                  psi_out(i+1,jj) = dpsi_full(I,J) + psi_out(i-1,jj-1)
                   chi_out(ip1,jj) = dchi_full(I,J) + chi_out(im1,jj-1)      
                 enddo
               ENDIF
@@ -126,7 +126,7 @@
                   im1 = iw(i)
                   ii = i + imb2
                   if (ii > im) ii = ii - im
-                  psi_out(ip1,j) = dpsi_full(I,J) + psi_out(im1,J-1)
+                  psi_out(i+1,j) = dpsi_full(I,J) + psi_out(i-1,J-1)
                   chi_out(ip1,j) = dchi_full(I,J) + chi_out(im1,J-1)             
                 enddo
               ELSE                                   !pole point,compute at jm-1
@@ -136,7 +136,7 @@
     		  print*,'line2',i+1,ip1
                   ip1 = ie(i)
                   im1 = iw(i)
-                  psi_out(ip1,jj) = dpsi_full(I,J) + psi_out(im1,jj-1)
+                  psi_out(i+1,jj) = dpsi_full(I,J) + psi_out(i-1,jj-1)
                   chi_out(ip1,jj) = dchi_full(I,J) + chi_out(im1,jj-1)              
                 enddo
               ENDIF
@@ -145,7 +145,7 @@
             DO I=2,IM-1
               ip1 = ie(i)
               im1 = iw(i)
-              psi_out(ip1,J-1) = dpsi_full(I,J) + psi_out(im1,J+1)
+              psi_out(i+1,J-1) = dpsi_full(I,J) + psi_out(i-1,J+1)
               chi_out(ip1,J-1) = dchi_full(I,J) + chi_out(im1,J-1)                 
             ENDDO
           END IF
