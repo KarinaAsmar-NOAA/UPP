1e2ba2c146db08e3dd28d922677f0f65b1491c71
KarinaAsmar-NOAA <karina.asmar@noaa.gov>
2024-10-21 12:45:47 +0000

    jesse's level fixes

--- sorc/ncep_post.fd/CALCHIPSI.f
+++ sorc/ncep_post.fd/CALCHIPSI.f
@@ -43,6 +43,7 @@
       use gridspec_mod, only: IDRT
       use ctlblk_mod, only: ISTA, IEND, JSTA, JEND, IM, JM, LSM, ME, SPVAL, MPI_COMM_COMP,&
                             num_procs, icnt, idsp, isxa, iexa, jsxa, jexa
+      use rqstfld_mod, only: IGET, LVLS
 !
 !- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
       implicit none
@@ -57,8 +58,8 @@
 
       integer k, m
       real, allocatable :: CHI1(:),CHISUB(:),PSI1(:),PSISUB(:),COL_UWIND(:,:),COL_VWIND(:,:),  		&
-      			   IN_UWIND(:,:,:),IN_VWIND(:,:,:),OUT_UWIND(:,:,:),OUT_VWIND(:,:,:), 		&
-	    		   DIV(:,:,:),ZO(:,:,:),CHI_OUT(:,:,:),PSI_OUT(:,:,:)
+      			   IN_UWIND(:,:),IN_VWIND(:,:),OUT_UWIND(:,:),OUT_VWIND(:,:), 		&
+	    		   DIV(:,:),ZO(:,:),CHI_OUT(:,:),PSI_OUT(:,:)
       
 !     
 !***************************************************************************
@@ -68,44 +69,39 @@
 
       ALLOCATE(COL_UWIND(IM,JM))
       ALLOCATE(COL_VWIND(IM,JM))
-      ALLOCATE(IN_UWIND(IM,JM,LSM))
-      ALLOCATE(IN_VWIND(IM,JM,LSM))
+
+      ALLOCATE(IN_UWIND(IM,JM))
+      ALLOCATE(IN_VWIND(IM,JM))
+      ALLOCATE(OUT_UWIND(IM,JM))
+      ALLOCATE(OUT_VWIND(IM,JM))
+      ALLOCATE(DIV(IM,JM))
+      ALLOCATE(ZO(IM,JM))
+      ALLOCATE(CHI_OUT(IM,JM))
+      ALLOCATE(PSI_OUT(IM,JM))
+
+      ALLOCATE(CHI1(im*jm))
+      ALLOCATE(CHISUB(icnt(me)))
+      ALLOCATE(PSI1(im*jm))
+      ALLOCATE(PSISUB(icnt(me)))
+
+      CHI = SPVAL
+      PSI = SPVAL
 
       DO L=1,LSM
+       IF(LVLS(L,IGET(1021)) > 0)THEN
+
         CALL COLLECT_ALL(UISO(ISTA:IEND,JSTA:JEND,L),COL_UWIND)
         CALL COLLECT_ALL(VISO(ISTA:IEND,JSTA:JEND,L),COL_VWIND)
 !$omp  parallel do private(i,j)
         DO J=1,JM
           DO I=1,IM
-	    IN_UWIND(I,J,L)=COL_UWIND(I,J)
-      	    IN_VWIND(I,J,L)=COL_VWIND(I,J)
+	    IN_UWIND(I,J)=COL_UWIND(I,J)
+      	    IN_VWIND(I,J)=COL_VWIND(I,J)
           ENDDO
         ENDDO
-      ENDDO
-
-      DEALLOCATE(COL_UWIND)
-      DEALLOCATE(COL_VWIND)
-
-! FILL CHI/PSI VALUES
-      DO L=1,LSM
-!$omp  parallel do private(i,j)
-      DO J=1,JM
-	  DO I=1,IM
-     	    CHI(I,J,L)= SPVAL
-	    PSI(I,J,L)= SPVAL
-     	   ENDDO
-	ENDDO
-      ENDDO
 
       IF (ME==0) THEN 
 
-	      ALLOCATE(OUT_UWIND(IM,JM,LSM))
-	      ALLOCATE(OUT_VWIND(IM,JM,LSM))
-	      ALLOCATE(DIV(IM,JM,LSM))
-	      ALLOCATE(ZO(IM,JM,LSM))
-	      ALLOCATE(CHI_OUT(IM,JM,LSM))
-	      ALLOCATE(PSI_OUT(IM,JM,LSM))
-      
         ! SET MAX WAVELENGTH FOR SPECTRAL TRUNCATION
 	      IF(IDRT == 0)THEN
 	        JCAP = (JM-3)/2
@@ -115,41 +111,26 @@
 
 	      ! COMPUTE CHI/PSI FROM WIND VECTORS IN SPECTRAL SPACE
  	      CALL SPTRUNV(0,JCAP,IDRT,IM,						             &
-  		           JM,IDRT,IM,JM,LSM,						             &
+  		           JM,IDRT,IM,JM,1,						             &
 	                   0,0,0,0,							             &
 	                   0,0,0,0,							             &
-	                   IN_UWIND(1,1,1),IN_VWIND(1,1,1),				             &
-	      	           .FALSE.,OUT_UWIND(1,1,1),OUT_VWIND(1,1,1),			  	     &
+	                   IN_UWIND(1,1),IN_VWIND(1,1),				             &
+	      	           .FALSE.,OUT_UWIND(1,1),OUT_VWIND(1,1),			  	     &
 	                   .FALSE.,DIV,ZO,						             &
-	                   .TRUE.,CHI_OUT(1,1,1),PSI_OUT(1,1,1))
-
-	      DEALLOCATE(IN_UWIND)
-       	      DEALLOCATE(IN_VWIND)
-      	      DEALLOCATE(OUT_UWIND)
-	      DEALLOCATE(OUT_VWIND)
-              DEALLOCATE(DIV)
-	      DEALLOCATE(ZO)
+	                   .TRUE.,CHI_OUT(1,1),PSI_OUT(1,1))
 
       ENDIF                             ! END OF ME=0 BLOCK
 
       CALL MPI_BARRIER(MPI_COMM_COMP, IERR)
 
-      ALLOCATE(CHI1(im*jm))
-      ALLOCATE(CHISUB(icnt(me)))
-      ALLOCATE(PSI1(im*jm))
-      ALLOCATE(PSISUB(icnt(me)))
-
-      DO L=1,LSM
-
          IF (ME==0) THEN
            k=0
            DO m=0,num_procs-1
-!$omp  parallel do private(i,j)
            DO J=jsxa(m),jexa(m)
            DO I=isxa(m),iexa(m)
               k=k+1
-              CHI1(k)=CHI_OUT(I,J,L)
-              PSI1(k)=PSI_OUT(I,J,L)
+              CHI1(k)=CHI_OUT(I,J)
+              PSI1(k)=PSI_OUT(I,J)
            ENDDO
            ENDDO
            ENDDO
@@ -160,9 +141,7 @@
          CALL MPI_SCATTERV(PSI1,icnt,idsp,MPI_REAL, &
                            PSISUB,icnt(me),MPI_REAL,0,MPI_COMM_WORLD,IERR)
 
- 
          k=0
-!$omp  parallel do private(i,j)
          DO J=JSTA,JEND
          DO I=ISTA,IEND
             k=k+1
@@ -171,14 +150,25 @@
          ENDDO
          ENDDO
 
+        ENDIF
       ENDDO
   
       DEALLOCATE(CHI1)
       DEALLOCATE(CHISUB)
       DEALLOCATE(PSI1)
       DEALLOCATE(PSISUB)
-      IF(ALLOCATED(CHI_OUT)) DEALLOCATE(CHI_OUT)
-      IF(ALLOCATED(PSI_OUT)) DEALLOCATE(PSI_OUT)
+
+      DEALLOCATE(IN_UWIND)
+      DEALLOCATE(IN_VWIND)
+      DEALLOCATE(OUT_UWIND)
+      DEALLOCATE(OUT_VWIND)
+      DEALLOCATE(DIV)
+      DEALLOCATE(ZO)
+      DEALLOCATE(CHI_OUT)
+      DEALLOCATE(PSI_OUT)
+
+      DEALLOCATE(COL_UWIND)
+      DEALLOCATE(COL_VWIND)
 !
 !     
 !     END OF ROUTINE.
